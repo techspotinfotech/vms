4575e5d6ed358455b7efc7353133a6de70ead8c5
Techspot infotech <121566496+techspotinfotech@users.noreply.github.com>
2025-11-27 22:59:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,11 @@
+import os
+
+SECRET_KEY = "techspot_vms_secret_123"
+
 DB_CONFIG = {
-    'host': 'sql12.freesqldatabase.com',
-    'user': 'sql12809682',
-    'password': 'mPFBKnjapx',
-    'database': 'sql12809682',
-    'port': 3306
+    'host': os.getenv("DB_HOST", "sql12.freesqldatabase.com"),
+    'user': os.getenv("DB_USER", "sql1278945"),
+    'password': os.getenv("DB_PASS", "9sd8sd8sd8"),
+    'database': os.getenv("DB_NAME", "sql1278945"),
+    'port': int(os.getenv("DB_PORT", 3306))
 }
