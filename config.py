4a23438cd6fbf0abbaabfd2a8de5ed7afa7593d0
Techspot infotech <121566496+techspotinfotech@users.noreply.github.com>
2025-11-27 22:37:52 +0530

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-DB_CONFIG = {
-    'host': os.getenv("DB_HOST"),
-    'user': os.getenv("DB_USER"),
-    'password': os.getenv("DB_PASS"),
-    'database': os.getenv("DB_NAME"),
-    'port': int(os.getenv("DB_PORT", 3306))
-}
