6e2aec9b7475bd428cee28ef746cd1797f9a4d74
Techspot infotech <121566496+techspotinfotech@users.noreply.github.com>
2025-11-27 22:52:29 +0530

    Update database.py

--- database.py
+++ database.py
@@ -2,9 +2,15 @@ import mysql.connector
 from config import DB_CONFIG
 
 def get_connection():
-    return mysql.connector.connect(
-        host=DB_CONFIG['host'],
-        user=DB_CONFIG['user'],
-        password=DB_CONFIG['password'],
-        database=DB_CONFIG['database']
-    )
+    try:
+        conn = mysql.connector.connect(
+            host=DB_CONFIG['host'],
+            user=DB_CONFIG['user'],
+            password=DB_CONFIG['password'],
+            database=DB_CONFIG['database'],
+            port=DB_CONFIG.get('port', 3306)  # cloud DB needs port
+        )
+        return conn
+    except Exception as e:
+        print("DATABASE CONNECTION ERROR:", e)
+        return None
